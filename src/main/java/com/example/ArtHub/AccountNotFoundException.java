036662dedce94011fa5869f22518a9e1a9d75cf1
NET_02_NguyenHiepPhu <phunhse173268@fpt.edu.vn>
2024-02-20 08:41:57 +0700

    test

--- src/main/java/com/example/ArtHub/AccountNotFoundException.java
+++ src/main/java/com/example/ArtHub/AccountNotFoundException.java
@@ -2,6 +2,6 @@ package com.example.ArtHub;
 
 public class AccountNotFoundException extends RuntimeException{
     public AccountNotFoundException(int id){
-        super("Instructor with ID:"+id+"do not existed!!");
+        super("Instructor with ID+1+2:"+id+"do not existed!!");
     }
 }
